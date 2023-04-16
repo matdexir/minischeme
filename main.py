86e021ccf84d7b40825df16d28d88656e3660ff5
matdexir <matthieudesir21@gmail.com>
2023-04-16 23:35:13 +0800

    feat: added the environment for a functional lisp

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+import operator as op
 from typing import Union
 
 Symbol = str
@@ -6,6 +8,49 @@ Atom = Union[Symbol, Number]
 List = list
 Exp = Union[Atom, List]
 Env = dict
+
+
+def standard_env() -> Env:
+    # defining an environment with some scheme standards
+    env = Env()
+    env.update(vars(math))
+    env.update(
+        {
+            "+": op.add,
+            "-": op.sub,
+            "*": op.mul,
+            "/": op.truediv,
+            ">": op.gt,
+            "<": op.lt,
+            ">=": op.ge,
+            "<=": op.le,
+            "=": op.eq,
+            "abs": abs,
+            "append": op.add,
+            "apply": lambda proc, args: proc(*args),
+            "begin": lambda *x: x[-1],
+            "car": lambda x: x[0],
+            "cdr": lambda x: x[1:],
+            "cons": lambda x, y: [x] + y,
+            "eq?": op.is_,
+            "expt": pow,
+            "equal?": op.eq,
+            "length": len,
+            "list": lambda *x: List(x),
+            "list?": lambda x: isinstance(x, List),
+            "map": map,
+            "max": max,
+            "min": min,
+            "not": op.not_,
+            "null?": lambda x: x == [],
+            "number?": lambda x: isinstance(x, Number),
+            "print": print,
+            "procedure?": callable,
+            "round": round,
+            "symbol?": lambda x: isinstance(x, Symbol),
+        }
+    )
+    return env
 
 
 def tokenize(chars: str) -> list:
